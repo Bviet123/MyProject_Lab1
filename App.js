5d73442fee8cd613b71ded0223ade66a71a5e628
Bviet123 <140326798+Bviet123@users.noreply.github.com>
2024-05-08 23:47:09 +0700

    App.js

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './Screens/HomeScreen';
-import MyStack from './Router/MyStack';
-import ProfileScreen from './Screens/ProfileSceens';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import MyDrawer from './Router/MyDrawer';
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LoginScreen from './src/LoginScreen';
+import HomeScreen from './src/HomeScreen';
+import RegistrationScreen from './src/RegistrationScreen';
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
-const App= ()=> {
-  return(
-    //B1 :
-      <NavigationContainer>
-        <MyStack/>
-      </NavigationContainer>
+const Stack = createStackNavigator();
 
-  )
+export default function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Registration" component={RegistrationScreen} />
+              <Stack.Screen name='Home' component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-export default App
-
